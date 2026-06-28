06af2641628914edb01a3e78cd1ae0ced7747fdf
Claudia Cachay-Osorio <52023117+claudiacachayosorio@users.noreply.github.com>
2026-06-28 14:06:00 -0400

    database schema

---
+++ schema.sql
@@ -0,0 +1,20 @@
+-- World Cup Database Schema
+
+CREATE DATABASE worldcup;
+
+CREATE TABLE teams(
+	team_id SERIAL PRIMARY KEY,
+	name VARCHAR(60) UNIQUE NOT NULL
+);
+
+CREATE TABLE games(
+	game_id SERIAL PRIMARY KEY,
+	year INT NOT NULL,
+	round VARCHAR(60) NOT NULL,
+	winner_id INT NOT NULL,
+	opponent_id INT NOT NULL,
+	winner_goals INT NOT NULL,
+	opponent_goals INT NOT NULL,
+	FOREIGN KEY (winner_id) REFERENCES teams(team_id),
+	FOREIGN KEY (opponent_id) REFERENCES teams(team_id)
+);
